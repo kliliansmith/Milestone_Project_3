35b564492ec60c4d4ef52018d0b8ab6ee4168c62
kliliansmith <kliliansmith@outlook.com>
2021-01-05 00:28:26 +0000

    add materalize validation

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,38 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('select').formSelect();
+
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        /* set 2 new variables to set css style to match materialize validation */
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        /* if a select element has property of required, then un-hide it with no width or height */
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        /* once user focused, traverse DOM up or down with event listeners */
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            /* when item selected, apply styles to make valid and green */
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            /* apply green valid class again */
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        /* apply invalid red class when no category has been selected */
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
